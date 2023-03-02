3193820bdbcac234368ce583e3e9f825ed8fd89c
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2023-03-01 23:55:15 -0800

    adjusted admin display

--- blog/admin.py
+++ blog/admin.py
@@ -3,5 +3,6 @@ from .models import BlogPost
 
 # Register your models here.
 
-
-admin.site.register(BlogPost)
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display =("title", "post")
