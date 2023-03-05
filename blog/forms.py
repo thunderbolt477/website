6b5dd348262d93912a4a10f10b28b7954f6a10c5
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2023-03-04 20:34:34 -0800

    adjust routing, adjusted models so that widgets could be used easier.

--- blog/forms.py
+++ blog/forms.py
@@ -1,13 +1,17 @@
-from django.forms import ModelForm, Textarea
+from django import forms
 from .models import BlogPost
 
-class BlogPostForm(ModelForm):
-    class Meta:
-        model = BlogPost
-        fields = ('title', 'post')
-        widgets = {
-            'post':Textarea(attrs={'cols':80, 'rows':20})
-        }
+class BlogPostForm(forms.Form):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
+    post = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'New Post','class':'form-control'}))
+    
+    
+    # class Meta:
+    #     model = BlogPost
+    #     fields = ('title', 'post')
+    #     widgets = {
+    #         'post':Textarea(attrs={'cols':80, 'rows':20},{class=''})
+    #     }
 
     # title = forms.CharField(label="Title", max_length=30)
     # post = forms.CharField(label="Please write your post here", widget=forms.Textarea)
