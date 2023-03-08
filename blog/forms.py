b49d3198c33ff0ea5ff530e925d63a32c1f0e0e5
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2023-03-07 23:53:28 -0800

    adjusted model form to use meta class. adjusted submission form.

--- blog/forms.py
+++ blog/forms.py
@@ -1,17 +1,27 @@
 from django import forms
 from .models import BlogPost
+from django.forms import ModelForm, TextInput, Textarea
 
-class BlogPostForm(forms.Form):
-    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 300px;', 'class': 'form-control'}))
-    post = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'New Post','class':'form-control'}))
+class BlogPostForm(ModelForm):  
+    class Meta:
+        model = BlogPost
+        fields = ['title', 'post']
+        widgets = {
+            'title': TextInput(attrs={
+                'class':'form-control',
+                'style': 'max-width: 300px;',
+                'placeholder':'Title'
+            }),
+            'post': Textarea(attrs={
+                'class':'form-control',
+                'placeholder':'Write your post here'
     
-    
-    # class Meta:
-    #     model = BlogPost
-    #     fields = ('title', 'post')
-    #     widgets = {
-    #         'post':Textarea(attrs={'cols':80, 'rows':20},{class=''})
-    #     }
+            })
+        }
 
+
+    # title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Title', 'style': 'width: 300px;', 'class': 'form-control'}))
+    # post = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'New Post','class':'form-control'}))
+    
     # title = forms.CharField(label="Title", max_length=30)
     # post = forms.CharField(label="Please write your post here", widget=forms.Textarea)
